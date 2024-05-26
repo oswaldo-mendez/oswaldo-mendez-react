c2174b05239863e3766dff73293a40b1c8e4ce57
oswaldo-mendez <ing.oswaldomendez@gmail.com>
2024-05-26 17:37:42 -0500

    fin de actividad cuarta clase

--- firstProjectRead/src/components/CharacterCard.tsx
+++ firstProjectRead/src/components/CharacterCard.tsx
@@ -7,9 +7,9 @@ function CharacterCard(props) {
 
         <div className="card">
             <div className="personaje">
-                <img className='imagen_personaje' src={props.url} alt="Foto de el artista/personaje" />;
+                <img className='imagen_personaje' src={props.url} alt="Foto de el artista/personaje" /><br />
                 <div className="detalles">
-                    <h1>{props.title}</h1>;
+                    <h3>{props.title}</h3><br />
                     <p>
                         <b>Genero:</b> {props.genre}
                     </p>
