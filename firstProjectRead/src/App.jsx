66a996af7a893296301f0262f88f8e22554df43b
oswaldo-mendez <ing.oswaldomendez@gmail.com>
2024-06-02 13:02:09 -0500

    fin de la actividd de clase 6 con filtros

--- firstProjectRead/src/App.jsx
+++ firstProjectRead/src/App.jsx
@@ -1,42 +1,86 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Character from './components/CharacterCard'
-import RickAndMortyCharacterCard from './components/RickAndMortyCharacterCard'
 
+//import RickAndMortyCharacterCard from './components/RickAndMortyCharacterCard'
 
 function App() {
-
   const [pagina, setPagina] = useState(1)
   const [pag, setPag] = useState()
   const [personajes, setPersonajes] = useState([])
-
-
-
+  const [nameFilter, setNameFilter] = useState('');
+  const [genreFilter, setGenreFilter] = useState('');
+  const [cambio, setCambio] = useState('')
+ 
 
   useEffect( () => {
     fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)
-      .then((response) => response.json())
-      .then((result) => {
-        setPersonajes(result.results)
-        setPag(result.info)
-      })
-      
+    .then((response) => response.json())
+    .then((result) => {
+      setPersonajes(result.results)
+      setPag(result.info)
+    })     
   }, [pagina])
+  
 
+  useEffect (() => {
+    const url= new URL(window.location.href)
+    const params= new URLSearchParams(url.search)
+    const page= params.get('page')
+    if(page){
+      setPagina(parseInt(page))
+    }
+  }, [])
 
+  useEffect( () => {
+    
+    fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}&name=${nameFilter}&gender=${genreFilter}`)
+    .then((response) => response.json())
+    .then((result) => {
+      setPersonajes(result.results)
+      setPag(result.info)
+    })
     console.log(pag)
-    const incrementarContador = () => {
-      if (pagina==pag.pages) setPagina(1);
-      if (pagina != pag.pages) setPagina(pagina + 1)
-      document.querySelector('h1').scrollIntoView({ behavior: 'smooth' });
+    console.log(personajes)
+}, [cambio,pagina])
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log('ok')
+    setCambio('ok')
+  }
+ 
+//botones para incrementar o decrementar las paginas
+  const incrementarContador = () => {
+    const url= new URL(window.location.href)
+
+    if (parseInt(pagina)==pag.pages) {
+      url.searchParams.set('page', 1)
+      window.history.replaceState({},'',url)
+      setPagina(1);
     }
-    const decrementarContador = () => {
-      if (pagina>=2) setPagina(pagina - 1);
-      if (pagina==1) setPagina(pag.pages);
-      
-      
-      document.querySelector('h1').scrollIntoView({ behavior: 'smooth' });
+    if (parseInt(pagina) != pag.pages){
+      url.searchParams.set('page', parseInt(pagina)+1)
+      window.history.replaceState({},'',url)
+      setPagina(parseInt(pagina)+1)
     }
+    document.querySelector('h1').scrollIntoView({ behavior: 'smooth' });
+  }
+  const decrementarContador = () => {
+    const url= new URL(window.location.href)
+    if (parseInt(pagina)>=2) {
+      url.searchParams.set('page', parseInt(pagina)-1)
+      window.history.replaceState({},'',url)
+      setPagina(parseInt(pagina)-1)
+    }
+    if (parseInt(pagina)==1) {
+      url.searchParams.set('page', pag.pages)
+      window.history.replaceState({},'',url)
+      setPagina(pag.pages);
+    }
+    document.querySelector('h1').scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
 
     <div>
@@ -44,17 +88,29 @@ function App() {
       {/*
       <RickAndMortyCharacterCard id={1}/>
       */}
+      <form onSubmit={handleSubmit}>
+            <div>
+                <input value={nameFilter} onChange={(e) => setNameFilter(e.target.value)}  type="text" placeholder='Search changer name' />
+            </div>
+            <div>
+                <input value={genreFilter} onChange={(e) => setGenreFilter(e.target.value)}  type="text" placeholder='Select genre' />
+            </div>
+
+            <button type="submit">Buscar</button>
+
+      </form>
        
-         <div className="container">
+        <div className="container">
           {personajes.map((personaje) => (
             <Character key={personaje.id} title={personaje.name} url={personaje.image} genre={personaje.gender} status={personaje.status} />
           ))}
-      </div>
+        </div>
+      {
       <div className="container2">
         <button className='paginaBtn'  onClick={decrementarContador} >pagina anterior</button>
         <button className='paginaBtn'  onClick={incrementarContador} >siguiente pagina</button>
       </div>
-      
+        }
      
     </div>
   )
